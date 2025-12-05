520ff82724b9dd7cf7e604523877a1730c8053f0
elenaalkoli <alkolielena@gmail.com>
2025-12-05 20:12:49 +0300

    completed customersApiService and schemas for getSorted and getCustomerOrders

---
+++ src/data/schemas/customers/getCustomerOrders.schema.ts
@@ -0,0 +1,15 @@
+import { obligatoryFieldsSchema, obligatoryRequredFields } from '../core.schema';
+import { orderSchema } from '../orders/order.schema';
+
+export const getCustomerOrdersSchema = {
+  type: 'object',
+  properties: {
+    Orders: {
+      type: 'array',
+      items: orderSchema,
+    },
+    ...obligatoryFieldsSchema,
+  },
+  required: ['Orders', ...obligatoryRequredFields],
+  additionalProperties: false,
+};
