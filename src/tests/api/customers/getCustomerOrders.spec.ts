14eec751ec5e7e393e3bba436588d4699d26e78a
elenaalkoli <alkolielena@gmail.com>
2025-12-12 12:02:32 +0300

    update getCustomerOrders tests on feature branch

--- src/tests/api/customers/getCustomerOrders.spec.ts
+++ src/tests/api/customers/getCustomerOrders.spec.ts
@@ -1,40 +1,50 @@
 import { test, expect } from 'fixtures/business.fixture';
 import { TAGS } from 'data/tags';
 import { customerOrdersSchema } from 'data/schemas/orders/order.schema';
-import { generateCustomerOrdersResponse } from 'data/salesPortal/orders/generateOrderData';
 import { validateJsonSchema } from 'utils/validation/validateSchema.utils';
+import { ICustomerOrdersResponse } from 'data/types/customer.types';
 
 test.describe('[API] [Sales Portal] [Customers]', () => {
-  const ids: string[] = [];
-  const token = '';
+  const createdCustomerIds: string[] = [];
+  let token = '';
+
+  test.beforeAll(async ({ loginApiService }) => {
+    token = await loginApiService.loginAsAdmin();
+  });
 
   test.afterEach(async ({ customersApiService }) => {
-    if (ids.length) {
-      for (const id of ids) {
+    if (createdCustomerIds.length) {
+      for (const id of createdCustomerIds) {
         await customersApiService.delete(token, id);
       }
-      ids.length = 0;
+      createdCustomerIds.length = 0;
     }
   });
 
   test(
-    'Get Customer Orders - Schema Validation',
+    'Get Customer Orders - api call and schema validation',
     {
       tag: [TAGS.REGRESSION, TAGS.CUSTOMERS, TAGS.API],
-    },
-    async () => {
-      const customerId = '19308a48c1b03ca52a43f6a8'; // any id
-      const mockOrdersResponse = generateCustomerOrdersResponse(customerId, 3);
+    },async ({ customersApiService }) => {
+    const createResponse = await customersApiService.create(token);
+    const customerId = createResponse._id;
+    createdCustomerIds.push(customerId);
 
-      console.log('Generated mock data:', JSON.stringify(mockOrdersResponse, null, 2));
+//тут добавить новый ордер для кастомера
+// const order = await ordersApiService.createOrder(token, customerId);
 
-      validateJsonSchema(customerOrdersSchema, mockOrdersResponse);
+    const response = await customersApiService.getCustomerOrders(token, customerId);
 
-      expect(mockOrdersResponse.Orders).toHaveLength(3);
-      expect(mockOrdersResponse.IsSuccess).toBe(true);
-      expect(mockOrdersResponse.ErrorMessage).toBeNull();
+    expect(response).toBeDefined();
+    expect(response.IsSuccess).toBe(true);
+    expect(response.Orders).toBeInstanceOf(Array);
 
-      mockOrdersResponse.Orders.forEach(order => {
+    validateJsonSchema(customerOrdersSchema, response as ICustomerOrdersResponse);
+
+    if (response.Orders.length === 0) {
+    console.log("The customer has no orders");
+    } else {
+    response.Orders.forEach(order => {
         expect(order.customer).toBe(customerId);
         expect(order).toHaveProperty('_id');
         expect(order).toHaveProperty('status');
@@ -51,5 +61,6 @@ test.describe('[API] [Sales Portal] [Customers]', () => {
         }
       });
     }
+  }
   );
 });
