17dcdbbf422c859a33f93b00267e8baf912b1afc
Tatyana Moskvina <moskvina.ta77@gmail.com>
2025-12-17 22:32:57 +0400

    Added index.ts in orders

---
+++ src/ui/pages/orders/index.ts
@@ -0,0 +1,19 @@
+import { CommentsTab } from "./comments.tab";
+import { CustomerDetailsSection } from "./customerDetails.page";
+import { DeliveryTab } from "./delivery.tab";
+import { HistoryTab } from "./history.tab";
+import { OrderDetailsPage } from "./orderDetails.page";
+import { OrdersListPage } from "./ordersList.page";
+import { RequestedProductsSection } from "./requestedProducts.page";
+import { ScheduleDeliveryPage } from "./scheduleDelivery.page";
+
+export {
+  OrdersListPage,
+  OrderDetailsPage,
+  CommentsTab,
+  CustomerDetailsSection,
+  DeliveryTab,
+  HistoryTab,
+  RequestedProductsSection,
+  ScheduleDeliveryPage,
+};
