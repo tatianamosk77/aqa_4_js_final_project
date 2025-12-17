de6cb85b76bdb541e470acb7b1a77562616fe373
Tatyana Moskvina <moskvina.ta77@gmail.com>
2025-12-17 21:37:34 +0400

    Finished orderDetails.page.ts

--- src/ui/pages/orders/orderDetails.page.ts
+++ src/ui/pages/orders/orderDetails.page.ts
@@ -1,3 +1,4 @@
+import { logStep } from "utils/report/logStep.utils";
 import { SalesPortalPage } from "../sales-portal.page";
 
 export class OrderDetailsPage extends SalesPortalPage {
@@ -15,35 +16,67 @@ export class OrderDetailsPage extends SalesPortalPage {
   readonly assignedManagerLink = this.orderDetailsHeader.locator("#assigned-manager-link");
   readonly editManagerButton = this.assignedManagerContainer.filter({ hasText: "Edit" });
   readonly removeManagerButton = this.assignedManagerContainer.filter({ hasText: "Remove" });
-  readonly cancelOrder = this.orderDetailsHeader.locator("#cancel-order");
+  readonly cancelOrderButton = this.orderDetailsHeader.locator("#cancel-order");
   readonly refreshOrderButton = this.orderDetailsHeader.locator("#refresh-order");
-  readonly orderStatus = this.orderStatusBar.filter({ hasText: "Order Status" }); //todo
-  readonly totalPrice = this.orderStatusBar.filter({ hasText: "Total Price" }); //todo
-  readonly delivery = this.orderStatusBar.filter({ hasText: "Delivery" }); //todo
-  readonly createdOn = this.orderStatusBar.filter({ hasText: "Created On" }); //todo
-
-  readonly customerSection = this.page.locator("#customer-section");
-  readonly editCustomerButton = this.customerSection.locator("#edit-customer-pencil");
-  readonly customerData = this.customerSection.locator(".p-3");
-
-  readonly productSection = this.page.locator("#products-section");
-  readonly editProductButton = this.productSection.locator("#edit-products-pencil");
-  readonly productData = this.productSection.locator("#products-accordion-section");
+  readonly orderStatus = this.orderStatusBar.filter({ hasText: "Order Status" }).locator("..");
+  readonly totalPrice = this.orderStatusBar.filter({ hasText: "Total Price" }).locator("..");
+  readonly delivery = this.orderStatusBar.filter({ hasText: "Delivery" }).locator("..");
+  readonly createdOn = this.orderStatusBar.filter({ hasText: "Created On" }).locator("..");
+  readonly processOrderButton = this.orderDetailsHeader.locator("#process-order");
 
   readonly uniqueElement = this.title;
 
-  // @logStep("Fill in product form to update")
-  // async fillForm(productData: Partial<IProduct>) {
-  //     if (productData.name) await this.nameInput.fill(productData.name);
-  //     if (productData.manufacturer)
-  //         await this.manufacturerSelect.selectOption(productData.manufacturer);
-  //     if (productData.price) await this.priceInput.fill(productData.price.toString());
-  //     if (productData.amount) await this.amountInput.fill(productData.amount.toString());
-  //     if (productData.notes) await this.notesInput.fill(productData.notes);
-  // }
+  @logStep("Get order number")
+  async getOrderNumber() {
+    await this.orderNumber.innerText();
+  }
 
-  // @logStep("Click save button during update")
-  // async clickSave() {
-  //     await this.saveButton.click();
-  // }
+  @logStep("Click to select manager")
+  async clickToSelectManager() {
+    await this.clickToAssingManager.click();
+  }
+  @logStep("Process order")
+  async processOrder() {
+    await this.processOrderButton.click();
+  }
+  @logStep("Refresh order")
+  async refreshOrder() {
+    await this.refreshOrderButton.click();
+  }
+  @logStep("Cancel order")
+  async cancelOrder() {
+    await this.cancelOrderButton.click();
+  }
+  @logStep("Click to assigned manager")
+  async clickToAssignedManager() {
+    await this.assignedManagerLink.click();
+  }
+  @logStep("Edit assigned manager")
+  async editManager() {
+    await this.editManagerButton.click();
+  }
+  @logStep("Remove assigned manager")
+  async removeManager() {
+    await this.removeManagerButton.click();
+  }
+  @logStep("Get order status")
+  async getOrderStatus() {
+    return await this.orderStatus.innerText();
+  }
+  @logStep("Get total price")
+  async getTotalPrice() {
+    return await this.totalPrice.innerText();
+  }
+  @logStep("Get order status")
+  async getDeliveryDate() {
+    return await this.delivery.innerText();
+  }
+  @logStep("Get created on")
+  async getCreatedOn() {
+    return await this.createdOn.innerText();
+  }
+  @logStep("Back to orders list")
+  async backToOrdersList() {
+    return await this.backToOrdersLink.click();
+  }
 }
